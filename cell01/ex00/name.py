3c07be16732954571d14624ac82cd66bcd4db124
peamsamu <auto.panyawat@gmail.com>
2025-06-16 09:07:41 +0000

    00-01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Panyawat"
+last_name  = "Eamsamutsri"
+print(first_name,last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Panyawat"
+last_name  = "Eamsamutsri"
+Whole_name = first_name + " " + last_name
+print(Whole_name)
